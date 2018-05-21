aecef32580f5816433788a0e9cb071111c71b0f3
Pier Roberto Lucisano <pierroberto.lucisano@nttdata.com>
2018-05-21 17:30:04 +0200

    Style: add basic design

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", event => {
   const Chart =  require('chart.js')
-  // const chartPie = require('./mock.js')
+
+  Chart.Tooltip.positioners.custom = (elements, eventPosition) => {
+      console.log('custom', elements, 'eventsposition', eventPosition)
+      const { x, y } = eventPosition
+      return {
+          x,
+          y,
+      };
+  }
 
   const ctx = document.getElementById('myChart')
-  console.log('ct', ctx)
 
-  const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
-      }
-  })
-});
+  const type = 'bar'
+
+  const custom = (tooltipModel) => {
+    const tooltipEl = document.getElementById('chartjs-tooltip')
+    console.log('elem', tooltipEl)
+  }
+
+  const options = {
+    responsive: true,
+    animationDuration: 5000,
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true,
+            },
+        }],
+    },
+    animation: {
+      easing: 'easeOutBounce',
+      duration: 1500,
+    },
+
+    tooltips: {
+      enabled: true,
+      backgroundColor: '#5991AF',
+      opacity: 0.5,
+    },
+  }
+
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: '#5991AF',
+        borderColor: 'transparent',
+        borderWidth: 1
+    }],
+  }
+
+  const myChart = new Chart(ctx, { type, data, options })
+
+  return true
+})
