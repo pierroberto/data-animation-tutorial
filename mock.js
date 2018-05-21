ca487e106826b22f1c302c9cc949c7f157febd35
Pier Roberto Lucisano <pierroberto.lucisano@nttdata.com>
2018-05-21 15:24:32 +0200

    Feat: add cart.js

---
+++ mock.js
@@ -0,0 +1,26 @@
+const chartPie = [
+  {
+    label: 'telco',
+    value: 0.47,
+  },
+  {
+    label: 'finanlcial',
+    value: 0.22,
+  },
+  {
+    label: 'energy',
+    value: 0.12,
+  },
+  {
+    label: 'manifacturing',
+    value: 0.12,
+  },
+  {
+    label: 'public',
+    value: 0.7,
+  }
+]
+
+module.exports = {
+  chartPie,
+}
